97d9ef1088a96a5eca503d40eb99fe7eb02636da
HJ.Park <hyunjun19@gmail.com>
2016-02-26 23:43:28 +0900

    mermaid 기본 기능 테스트

--- desktop/app/main.js
+++ desktop/app/main.js
@@ -3,5 +3,21 @@ import $ from 'jquery';
 import mermaid from 'mermaid';
 
 console.log('main.js underscore version is', _.VERSION);
+console.log('main.js jquery     version is', $.fn.jquery);
+console.log('main.js mermaid    version is', mermaid.version());
 
-mermaid.initialize({startOnLoad:true});
+mermaidAPI.initialize({startOnLoad:false});
+
+$(function(){
+    // Example of using the API
+    window.$logicEditor   = $('#logic-editor');
+    window.$mermaidCanvas = $('#mermaid-canvas');
+
+    $mermaidCanvas.render = function(){
+        mermaidAPI.render('canvas', $logicEditor.val(), function(svgCode, bindFunctions){
+            $mermaidCanvas.html(svgCode);
+        });
+    };
+
+    $mermaidCanvas.render();
+});
