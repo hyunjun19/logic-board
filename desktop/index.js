dfc4e1fef3d9fb967e6e9b7602369bd19bbe6708
HJ.Park <hyunjun19@gmail.com>
2016-03-03 01:43:39 +0900

    마이너 오류 수정

--- desktop/index.js
+++ desktop/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const { app, BrowserWindow, dialog } = require('electron');
+const electron = require('electron');
+const app = electron.app;  // 어플리케이션 기반을 조작 하는 모듈.
+const BrowserWindow = electron.BrowserWindow;  // 네이티브 브라우저 창을 만드는 모듈.
+// const { app, BrowserWindow, dialog } = require('electron');
 
 // 윈도우 객체를 전역에 유지합니다. 만약 이렇게 하지 않으면
 // 자바스크립트 GC가 일어날 때 창이 멋대로 닫혀버립니다.
